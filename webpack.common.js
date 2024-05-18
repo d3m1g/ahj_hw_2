295ee095097e708c7116ddc1e78680ffe2f3d670
Alexander Sambur <demik86@mail.ru>
2024-05-19 01:06:50 +1000

    third

--- webpack.common.js
+++ webpack.common.js
@@ -55,21 +55,3 @@ module.exports = {
   }),
  ],
 };
-
-module.exports = {
- module: {
-  rules: [
-   {
-    test: /\.(png|jpg|gif)$/i,
-    use: [
-     {
-      loader: 'url-loader',
-      options: {
-       limit: 8192,
-      },
-     },
-    ],
-   },
-  ],
- },
-};
